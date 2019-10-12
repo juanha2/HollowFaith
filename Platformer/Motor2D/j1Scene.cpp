1a501ab912bf1cc3b19e3b880ea82fad3f25c2c5
juanha2 <47743754+juanha2@users.noreply.github.com>
2019-10-12 12:25:35 +0200

    Camera limits debug

--- Platformer/Motor2D/j1Scene.cpp
+++ Platformer/Motor2D/j1Scene.cpp
@@ -52,17 +52,27 @@ bool j1Scene::Update(float dt)
 	if(App->input->GetKey(SDL_SCANCODE_S) == KEY_DOWN)
 		App->SaveGame();		
 
-	if(App->input->GetKey(SDL_SCANCODE_UP) == KEY_REPEAT)
-		App->render->camera.y -= 1;
+	if (App->input->GetKey(SDL_SCANCODE_UP) == KEY_REPEAT) {	
+		if (App->render->camera.y>-App->map->data.height*App->map->data.tile_height)
+			App->render->camera.y -= 1;	
+	}		
 
-	if(App->input->GetKey(SDL_SCANCODE_DOWN) == KEY_REPEAT)
-		App->render->camera.y += 1;
+	if (App->input->GetKey(SDL_SCANCODE_DOWN) == KEY_REPEAT) {
+		if (App->render->camera.y < 0)
+			App->render->camera.y += 1;
+	}		
 
-	if(App->input->GetKey(SDL_SCANCODE_LEFT) == KEY_REPEAT)
-		App->render->camera.x -= 1;
+	if (App->input->GetKey(SDL_SCANCODE_LEFT) == KEY_REPEAT) {	
+		//LOG("%i", App->map->data.);
+		if(App->render->camera.x>-App->map->data.width*App->map->data.tile_width)
+			App->render->camera.x -= 1;
+	}		
 
-	if(App->input->GetKey(SDL_SCANCODE_RIGHT) == KEY_REPEAT)
-		App->render->camera.x += 1;
+	if (App->input->GetKey(SDL_SCANCODE_RIGHT) == KEY_REPEAT) {
+		if (App->render->camera.x < 0)
+			App->render->camera.x += 1;
+	}
+		
 
 	//App->render->Blit(img, 0, 0);
 	App->map->Draw();
